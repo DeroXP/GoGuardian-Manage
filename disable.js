2f8b0289bd734e75206a3539b1c11852ae7729fa
DeroXP <96676237+DeroXP@users.noreply.github.com>
2024-02-05 18:40:32 +0000

    Update disable.js

--- disable.js
+++ disable.js
@@ -1,18 +1,3 @@
-const extensionsToDisable = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingfgjhjeffij", "kmpjlilnemjciohjckjadmgmicoldglf", "ifeifkfohlobcbhmlfkenopaimbmnahb", "jaoebcikabjppaclpgbodmmnfjihdngk", "kbohafcopfpigkjdimdcdgenlhkmhbnc", "gcjpefhffmcgplgklffgbebganmhffje", "lgcbihdlknkcmmnapfocjbkdefkhmolo"];
-
-function disableExtension(extensionId, callback) {
-  chrome.management.setEnabled(extensionId, false, () => {
-    // Extension is now disabled
-    if (callback && typeof callback === 'function') {
-      callback(extensionId);
-    }
-  });
-}
-
-extensionsToDisable.forEach(extensionId => {
-  disableExtension(extensionId, (disabledExtensionId) => {
-    alert(`Extension ${disabledExtensionId} is now disabled.`);
-  });
+chrome.runtime.sendMessage({action: 'hideTab'}, function(response) {
+    alert('Success!!');
 });
-
-alert(`Success!!`);
